2abcf78e328a9ce31276e842465ffaaf7d1a0620
AleixTorresRodriguez <aleix.torresr@e-campus.uab.cat>
2020-05-06 15:43:44 +0200

    src/main.py

--- SimulacioRLP_py/src/main.py
+++ SimulacioRLP_py/src/main.py
@@ -27,7 +27,11 @@ from direct.task.Task import Task
 import sys
 from recognition import *
 import numpy as np
+import threading as th
 import speechRecognition as sr
+import _thread
+import threading
+vr = sr.VoiceRecognition(0)
 import cv2
 
 # Some constants for the program
@@ -36,10 +40,30 @@ MAX_SPEED = 5      # Max speed in ft/sec
 MAX_SPEED_SQ = MAX_SPEED ** 2  # Squared to make it easier to use lengthSquared
 # Instead of length
 
-vr = sr.VoiceRecognition()
+timerStatus = 'stop'
+
+def listenVoice():
+    while 1:
+        if vr.restart:
+            vr.restart = 0
+            sys.exit()
+
+        accio, coord = vr.recon_Voice()
+
+        if accio == 'a1':
+            print('Empezando partida')
+
+        if accio == 'a2':
+            print('parando')
+        if accio == 'a3':
+            print('Moviendo tablero a las coordenadas dichas')
+
+        if accio == 'a4':
+            print('Reiniciando...')
+
+
 
 class BallInMazeDemo(ShowBase):
-
     def __init__(self):
         # Initialize the ShowBase class from which we inherit, which will
         # create a window and set up everything we need for rendering into it.
@@ -235,10 +259,12 @@ class BallInMazeDemo(ShowBase):
         self.digitizer = Digitizer()
 
 
-        accio, coord = vr.recon_Voice()
+        #accio, coord = vr.recon_Voice()
 
         # Finally, we call start for more initialization
         self.start()
+
+
 
     def start(self):
         # The maze model also has a locator in it for where to start the ball
@@ -365,6 +391,9 @@ class BallInMazeDemo(ShowBase):
             elif name == "ground_collide":
                 self.groundCollideHandler(entry)
             elif name == "loseTrigger":
+                vr.restart=1
+                x = threading.Thread(target=listenVoice)
+                x.start()
                 self.loseGame(entry)
 
         # Read the mouse position and tilt the maze accordingly
@@ -423,6 +452,8 @@ class BallInMazeDemo(ShowBase):
         toPos = entry.getInteriorPoint(render)
         taskMgr.remove('rollTask')  # Stop the maze task
 
+
+
         # Move the ball into the hole over a short sequence of time. Then wait a
         # second and call start to reset the game
         Sequence(
@@ -438,4 +469,12 @@ class BallInMazeDemo(ShowBase):
 
 # Finally, create an instance of our class and start 3d rendering
 demo = BallInMazeDemo()
+
+try:
+    x = threading.Thread(target=listenVoice)
+    x.start()
+except:
+    print ("Error: unable to start thread")
+
+
 demo.run()
--- SimulacioRLP_py/src/speechRecognition.py
+++ SimulacioRLP_py/src/speechRecognition.py
@@ -1,85 +1,85 @@
 import speech_recognition as sr
+import sys
 
 class VoiceRecognition:
+    def __init__(self, restart):
+        self.restart=restart
+
 
     def recon_Voice(self):
         r = sr.Recognizer()
         coord=[]
         correcto1=1
-        correcto=1
         correcto2=1
+        accio = "none"
 
-        while correcto:
-            with sr.Microphone() as source:
-                print("Speak")
-                try:
-                    audio = r.listen(source)
-                    text = r.recognize_google(audio, language='es-ES')
-                    print(text)
+        with sr.Microphone() as source:
+            print("Speak")
+            try:
+                audio = r.listen(source)
+                text = r.recognize_google(audio, language='es-ES')
+                print(text)
 
-                    if 'empezar' == text or 'comenzar' == text:
-                        print("Game is runing")
-                        correcto=0
-                        accio = "a1"
-                    elif 'terminar' == text or 'parar' == text:
-                            print("Game finished")
-                            correcto = 0
-                            accio = "a2"
-                    elif 'coordenada' == text or 'coordenadas' == text:
-                        correcto = 0
-                        accio = "a3"
+                if 'empezar' == text or 'comenzar' == text:
+                    print("Game is runing")
+                    accio = "a1"
+                elif 'terminar' == text or 'parar' == text:
+                        print("Game finished")
+                        accio = "a2"
+                elif 'coordenada' == text or 'coordenadas' == text:
+                    accio = "a3"
 
-                        while correcto1:
-                            cX=input("Escribe la coordenada X: ")
-                            if int(cX) <= 100 and int(cX) >= 0:
-                                cY=input("Escribe la coordenada Y: ")
-                                if int(cY) <= 100 and int(cY) >= 0:
+                    while correcto1:
+                        cX=input("Escribe la coordenada X: ")
+                        if int(cX) <= 100 and int(cX) >= 0:
+                            cY=input("Escribe la coordenada Y: ")
+                            if int(cY) <= 100 and int(cY) >= 0:
+                                correcto1=0
+                                coord = [int(cX), int(cY)]
+                        """with sr.Microphone() as source:
+                            print("Di la coordenada X: ")
+
+                            try:
+                                audioX = r.listen(source)
+                                textX = r.recognize_google(audioX, language='es-ES')
+
+                                if textX <= 80 and textX >= 0: #Canviar numeros
                                     correcto1=0
-                                    coord = [int(cX), int(cY)]
-                            """with sr.Microphone() as source:
-                                print("Di la coordenada X: ")
 
-                                try:
-                                    audioX = r.listen(source)
-                                    textX = r.recognize_google(audioX, language='es-ES')
+                                    while correcto2:
+                                        with sr.Microphone() as source:
+                                            print("Di la coordenada Y: ")
 
-                                    if textX <= 80 and textX >= 0: #Canviar numeros
-                                        correcto1=0
+                                            try:
+                                                audioY = r.listen(source)
+                                                textY = r.recognize_google(audioY, language='es-ES')
 
-                                        while correcto2:
-                                            with sr.Microphone() as source:
-                                                print("Di la coordenada Y: ")
+                                                if textY <= 80 and textY >= 0: #canviar numeros
+                                                    correcto2=0
+                                                    coord=[int(textX), int(textY)]
 
-                                                try:
-                                                    audioY = r.listen(source)
-                                                    textY = r.recognize_google(audioY, language='es-ES')
+                                            except sr.UnknownValueError:
+                                                print('error')
+                                            except sr.RequestError as e:
+                                                print('failed'.format(e))
 
-                                                    if textY <= 80 and textY >= 0: #canviar numeros
-                                                        correcto2=0
-                                                        coord=[int(textX), int(textY)]
+                            except sr.UnknownValueError:
+                                print('error')
+                            except sr.RequestError as e:
+                                print('failed'.format(e))"""
 
-                                                except sr.UnknownValueError:
-                                                    print('error')
-                                                except sr.RequestError as e:
-                                                    print('failed'.format(e))
+                elif 'reiniciar' == text or 'reinicia' == text:
+                    accio = "a4"
 
-                                except sr.UnknownValueError:
-                                    print('error')
-                                except sr.RequestError as e:
-                                    print('failed'.format(e))"""
+                else:
+                     print("No hay comando asociado a esta palabra")
 
-                    elif 'reiniciar' == text or 'reinicia' == text:
-                        correcto = 0
-                        accio = "a4"
-
-                    else:
-                         print("No hay comando asociado a esta palabra")
-
-
-                except sr.UnknownValueError:
-                    print('Error, no se entendio la palabra')
-                except sr.RequestError as e:
-                    print('failed'.format(e))
+            except sr.UnknownValueError:
+                print('Error, no se entendio la palabra')
+                return accio, coord
+            except sr.RequestError as e:
+                print('failed'.format(e))
+                return accio, coord
         return accio,coord
 
 
