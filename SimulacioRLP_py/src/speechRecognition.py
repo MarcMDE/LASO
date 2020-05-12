b540f5873a976d8f31a3b203d3923f7a79efea97
AleixTorresRodriguez <aleix.torresr@e-campus.uab.cat>
2020-05-12 17:31:01 +0200

    canvis speech recognition finals

--- SimulacioRLP_py/src/speechRecognition.py
+++ SimulacioRLP_py/src/speechRecognition.py
@@ -21,77 +21,39 @@ class VoiceRecognition:
                 audio = r.listen(source)
                 text = r.recognize_google(audio, language='es-ES')
                 print(text)
-
-                if 'empezar' == text or 'comenzar' == text:
+                text = text.split()
+                if 'empezar' in text or 'comenzar' in text:
                     print("Game is runing")
                     accio = "a1"
-                elif 'terminar' == text or 'parar' == text:
+
+                elif 'terminar' in text or 'parar' in text:
                         print("Game finished")
                         accio = "a2"
-                elif 'coordenada' == text or 'coordenadas' == text:
-                    accio = "a3"
 
-                    while correcto1:
-                        cX=input("Escribe la coordenada X: ")
-                        if int(cX) <= 100 and int(cX) >= 0:
-                            cY=input("Escribe la coordenada Y: ")
-                            if int(cY) <= 100 and int(cY) >= 0:
-                                correcto1=0
-                                coord = [int(cX), int(cY)]
-                        """with sr.Microphone() as source:
-                            print("Di la coordenada X: ")
-
-                            try:
-                                audioX = r.listen(source)
-                                textX = r.recognize_google(audioX, language='es-ES')
-
-                                if textX <= 80 and textX >= 0: #Canviar numeros
-                                    correcto1=0
-
-                                    while correcto2:
-                                        with sr.Microphone() as source:
-                                            print("Di la coordenada Y: ")
-
-                                            try:
-                                                audioY = r.listen(source)
-                                                textY = r.recognize_google(audioY, language='es-ES')
-
-                                                if textY <= 80 and textY >= 0: #canviar numeros
-                                                    correcto2=0
-                                                    coord=[int(textX), int(textY)]
-
-                                            except sr.UnknownValueError:
-                                                print('error')
-                                            except sr.RequestError as e:
-                                                print('failed'.format(e))
-
-                            except sr.UnknownValueError:
-                                print('error')
-                            except sr.RequestError as e:
-                                print('failed'.format(e))"""
-
-                elif 'reiniciar' == text or 'reinicia' == text:
+                elif 'reiniciar' in text or 'reinicia' in text:
                     accio = "a4"
-                elif 'voz' == text:
+
+                elif 'cambiar' in text:
                     accio = "a5"
                     self.voice_solving = not self.voice_solving
                     self.dir = (0, 0)
+
                 elif self.voice_solving:
                     p = self.dir[0]
                     r = self.dir[1]
-                    if 'arriba' == text:
+                    if 'arriba' in text:
                         accio = "a6"
                         p = -1
-                    elif 'abajo' == text:
+                    if 'abajo' in text:
                         accio = "a6"
                         p = 1
-                    elif 'izquierda' == text:
+                    if 'izquierda' in text:
                         accio = "a6"
                         r = -1
-                    elif 'derecha' == text:
+                    if 'derecha' in text:
                         accio = "a6"
                         r = 1
-                    elif 'frenar' == text:
+                    if 'recto' in text:
                         accio = "a6"
                         p = 0
                         r = 0
